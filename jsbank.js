dce0749b7d3abc1938ab82e16ca9f2697a92000f
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2019-12-29 10:31:45 +1300

    Added compounding interest JS code
    Added compounding interest JS code & modified HTML file for better DOM manipulation

--- jsbank.js
+++ jsbank.js
@@ -1,15 +1,32 @@
-var chequeDiv = document.getElementsByClassName("cheque");
-var chequeBalance = chequeDiv.getElementByClassName("balance").getElementByClassName("amount");
+//get money str
+var chequeStr = document.getElementById("chequeBalance").innerHTML;
+var onlineStr = document.getElementById("onlineBalance").innerHTML; 
+var usdStr =document.getElementById("usdBalance").innerHTML; 
 
-var onlineBalance;
-var usdBalance;
+//get num str from "NZD $..." type of str
+function getAmount(moneyStr){
+    let dollarIndex = moneyStr.indexOf("$");
+    return moneyStr.slice(dollarIndex+1, moneyStr.length);
+}
+
+//convert each num str into int
+var chequeBalance = parseInt(getAmount(chequeStr));
+var onlineBalance = parseInt(getAmount(onlineStr));
+var usdBalance = parseInt(getAmount(usdStr));
 
 //function for compounding interest
-function compoundInterest(){
-    chequeBalance *= 1.1;
-    onlineBalance *= 1.1;
+function compoundInterest(chqBal, onlBal, chqStr, onlStr){
+    chqBal *= 1.02;
+    onlBal *= 1.04;
+    let chequeDollarIndex = chqStr.indexOf("$");
+    let onlineDollarIndex = onlStr.indexOf("$");
+    //update money str
+    chqStr = chqStrchqStr.slice(0, chequeDollarIndex +1) + chqBal.toString();
+    onlStr = onlStr.slice(0, onlineDollarIndex +1) + onlBal.toString();
 }
-setTimeout(compoundInterest(), 5000);
+
+//the chq&online balance compounds every 1000ms by calling the compound interest function
+setTimeout(compoundInterest(chequeBalance, onlineBalance, chequeStr, onlineStr), 1000);
 
 //function for money transfer
 function transfer(){
